d3f4e8741664a3484f2233a4c80fd8000585a446
Brenda Huemer <89327840+brxndxz@users.noreply.github.com>
2023-05-05 23:50:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const monedaEl_one = document.getElementById('moneda-uno');
+const monedaEl_two = document.getElementById('moneda-dos');
+const cantidadEl_one = document.getElementById('cantidad-uno');
+const cantidadEl_two = document.getElementById('cantidad-dos');
+const cambioEl = document.getElementById('cambio');
+const tasaEl = document.getElementById('tasa');
+
+
+// Fetch Exchange Rate and Update the DOM
+function calculate(){
+    const moneda_one = monedaEl_one.value;
+    const moneda_two = monedaEl_two.value;
+
+   fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
+   .then(res => res.json() )
+   .then(data => {
+       const tasa = data.rates[moneda_two];
+       
+       cambioEl.innerText = `1 ${moneda_one} = ${tasa} ${moneda_two}`;
+
+       cantidadEl_two.value = (cantidadEl_one.value * tasa).toFixed(2);
+
+    } );
+    
+}
+monedaEl_one.addEventListener('change', calculate);
+cantidadEl_one.addEventListener('input', calculate);
+monedaEl_two.addEventListener('change', calculate);
+cantidadEl_two.addEventListener('input', calculate);
+
+tasa.addEventListener('click', () =>{
+    const temp = monedaEl_one.value;
+    monedaEl_one.value = monedaEl_two.value;
+    monedaEl_two.value = temp;
+    calculate();
+} );
+
+
+calculate();
